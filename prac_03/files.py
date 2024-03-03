3dd16f7cae366118444c4498022e0cc671edeec3
LI HEWEI <hewei.li@my.jcu.edu.au>
2024-03-03 19:50:39 +0800

    Add prac 3 file

---
+++ prac_03/files.py
@@ -0,0 +1,21 @@
+with open("name.txt", "r") as file:
+    line = file.readline()
+    while line:
+        print(line.strip())
+        line = file.readline()
+
+
+with open("numbers.txt", "r") as file:
+    lines = file.readlines()
+if len(lines) < 2:
+    print("least 2 lines.")
+else:
+    # Strip removes leading and trailing spaces, split splits strings into lists based on spaces
+    line_one = lines[0].strip().split()
+    line_two = lines[1].strip().split()
+
+    # Convert to integers and add them up
+    result = [str(int(a) + int(b)) for a, b in zip(line_one, line_two)]
+
+    # Connect elements in the result list with spaces and print
+    print(' '.join(result))
