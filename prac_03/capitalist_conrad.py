3a1e2dcf538a95b5a35c0672171bf337d5abbe6c
LI HEWEI <hewei.li@my.jcu.edu.au>
2024-03-03 20:54:27 +0800

    Add prac 3 file

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -21,15 +21,14 @@ with open('output.txt', 'w') as f:
     while MIN_PRICE <= price <= MAX_PRICE:
         price_change = 0
         random_int = random.randint(1, 2)
+        days += 1
         if random.randint(1, 2) == 1:
             price_change = random.uniform(0, MAX_INCREASE)
-            days += 1
         else:
             price_change = random.uniform(-MAX_DECREASE, 0)
-            days += 1
 
         price *= (1 + price_change)
-        print(f"On day {i} price is:${price:,.2f}")
+        print(f"On day {days} price is:${price:,.2f}")
         f.write(str(price) + '\n')
 
 print("txt have been writen")
