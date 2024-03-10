b78040d8741410eaf3baf32b21db14091157b668
LI HEWEI <hewei.li@my.jcu.edu.au>
2024-03-10 20:50:17 +0800

    Add prac 4 files

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,34 +8,34 @@ NUM_PICKS_PER_LINE = 6
 
 def generate_quickly_picks(num_lines):
     quickly_picks = []
-
     for _ in range(num_lines):
-        # Generate a list of numbers from 1 to 45
-        numbers = list(range(MIN_NUMBER, MAX_NUMBER + 1))
+        for _ in range(num_lines):
+            pick = []
+            while True:
+                i = len(pick)
+                a_num = random.randint(MIN_NUMBER, MAX_NUMBER + 1)
+                if a_num in pick:
+                    continue
+                else:
+                    if i < NUM_PICKS_PER_LINE:
+                        pick.append(a_num)
+                    else:
+                        break
+            pick.sort()
+            quickly_picks.append(pick)
 
-        # Shuffle the list of numbers
-        random.shuffle(numbers)
-
-        # Take the first 6 numbers from the shuffled list
-        pick = numbers[:NUM_PICKS_PER_LINE]
-
-        # Sort the numbers in ascending order
-        pick.sort()
-
-        # Append the pick to the list of quick picks
-        quickly_picks.append(pick)
-
-    return quickly_picks
+        return quickly_picks
 
 
-def display_quick_picks(quickly_picks):
-    for pick in quick_picks:
+def display_quickly_picks(quickly_picks):
+    for pick in quickly_picks:
         # Format the numbers to align neatly
-        formatted_pick = ' '.join(f"{num:2}" for num in pick)
+        formatted_pick = ' '.join(f"{num:>3}" for num in pick)
         print(formatted_pick)
 
 
 # Main program
 num_quick_picks = int(input("How many quick picks? "))
-quick_picks = generate_quickly_picks(num_quick_picks)
-display_quick_picks(quick_picks)
+quickly_picks = generate_quickly_picks(num_quick_picks)
+display_quickly_picks(quickly_picks)
+
