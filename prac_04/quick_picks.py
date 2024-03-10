ebecfcc54b51c1484fbee9afdeeeb803e4ea08e5
LI HEWEI <hewei.li@my.jcu.edu.au>
2024-03-10 19:16:46 +0800

    Add prac 4 files

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,8 +6,8 @@ MAX_NUMBER = 45
 NUM_PICKS_PER_LINE = 6
 
 
-def generate_quick_picks(num_lines):
-    quick_picks = []
+def generate_quickly_picks(num_lines):
+    quickly_picks = []
 
     for _ in range(num_lines):
         # Generate a list of numbers from 1 to 45
@@ -23,12 +23,12 @@ def generate_quick_picks(num_lines):
         pick.sort()
 
         # Append the pick to the list of quick picks
-        quick_picks.append(pick)
+        quickly_picks.append(pick)
 
-    return quick_picks
+    return quickly_picks
 
 
-def display_quick_picks(quick_picks):
+def display_quick_picks(quickly_picks):
     for pick in quick_picks:
         # Format the numbers to align neatly
         formatted_pick = ' '.join(f"{num:2}" for num in pick)
@@ -37,5 +37,5 @@ def display_quick_picks(quick_picks):
 
 # Main program
 num_quick_picks = int(input("How many quick picks? "))
-quick_picks = generate_quick_picks(num_quick_picks)
+quick_picks = generate_quickly_picks(num_quick_picks)
 display_quick_picks(quick_picks)
