e63b187f6eb34779ec875d2ed575a8e053422a73
LI HEWEI <hewei.li@my.jcu.edu.au>
2024-03-17 15:35:09 +0800

    Add prac 5 files

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -32,7 +32,7 @@ def count_words(string):
     # Print the formatted word counts
     for word in sorted_words:
         count = word_counts[word]
-        formatted_word = word.ljust(len(longest_word))  # Left-justify the word to match the longest word's length
+        formatted_word = word.rjust(len(longest_word))  # Right-justify the word to match the longest word's length
         print(f"{formatted_word} : {count}")
 
 
