9596681f1871d66cc1b383ae3080d0d9e1c366ac
LI HEWEI <hewei.li@my.jcu.edu.au>
2024-04-14 23:52:12 +0800

    Add Prac 9 files

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,6 +1,7 @@
 import unittest
 from unreliable_car import UnreliableCar
 
+
 class TestUnreliableCar(unittest.TestCase):
     def test_drive_reliability(self):
         # Test driving with high reliability
@@ -15,6 +16,7 @@ class TestUnreliableCar(unittest.TestCase):
         # Test driving with fuel constraint
         car = UnreliableCar("Car", 20, 100)
         self.assertEqual(car.drive(30), 20)  # Should drive only 20 km due to fuel constraint
+
 
 if __name__ == "__main__":
     unittest.main()
