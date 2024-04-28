3739ac0d524dfe6a2628a441e732b761845f17be
LI HEWEI <hewei.li@my.jcu.edu.au>
2024-04-28 17:38:01 +0800

    Add Prac10 files

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -6,23 +6,27 @@ app = Flask(__name__)
 def celsius_to_fahrenheit(celsius):
     return celsius * 1.8 + 32
 
+
 @app.route('/')
 def hello_world():
     return '<h1>Hello World :)</h1>'
+
 
 @app.route('/greet')
 @app.route('/greet/<name>')
 def greet(name=""):
     return f"Hello {name}"
 
+
 @app.route('/convert/<celsius>')
 def convert_temperature(celsius):
     try:
         celsius_value = float(celsius)
         fahrenheit_value = celsius_to_fahrenheit(celsius_value)
-        return f"{celsius} degrees Celsius is equal to {fahrenheit_value:.2f} degrees Fahrenheit."
+        return f"The temperature of {celsius} degrees Celsius is equal to {fahrenheit_value:.2f} degrees Fahrenheit."
     except ValueError:
         return "Invalid input. Please enter a valid number for Celsius temperature."
+
 
 if __name__ == '__main__':
     app.run()
